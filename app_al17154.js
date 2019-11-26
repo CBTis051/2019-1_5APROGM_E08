c9066b0dc6820ec5a652ba42b0f49f6563df3278
Alejandro Iván Lizárraga Rojas <al17154@cbtis051.edu.mx>
2019-11-26 11:08:00 -0700

    Commit inicial del server

---
+++ app_al17154.js
@@ -0,0 +1,8 @@
+var http = require('http');
+http.createServer(function (req, res){
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+
+    res.end('Hola mundo Node Js Repo GIT - Alejandro Ivan Lizarraga Rojas\n');
+}).listen(8081);
+
+console.log('Server running on port 8081');
