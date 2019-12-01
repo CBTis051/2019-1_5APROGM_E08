ad80ae50744852abbf6e8f2a99685c003125a138
al17154 <al17154@cbtis051.edu.mx>
2019-12-01 14:52:32 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17154.js
+++ app_al17154.js
@@ -1,8 +1,16 @@
-var http = require('http');
-http.createServer(function (req, res){
-    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+var http = require("http");
+http.createServer(function(req, res) {
 
-    res.end('Hola mundo Node Js Repo GIT - Alejandro Ivan Lizarraga Rojas\n');
-}).listen(8081);
+    res.writeHead(200, { "Content-Type": "application/json; charset=utf-8" });
 
-console.log('Server running on port 8081');
+    var objeto = {
+        mensaje : 'Hola mundo Node Js Repo GIT - Alejandro Ivan Lizarraga Rojas\n'
+    };
+
+    var json = JSON.stringify(objeto);
+
+    res.end(json);
+    
+  }).listen(8081);
+
+console.log("Server running on port 8081");
